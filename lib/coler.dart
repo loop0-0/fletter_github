ebb4a3e557838f3c7d238a4b9f0a836462a43ba4
loop <spm6220@gmail.com>
2023-02-15 00:11:30 +0100

    add style classe

---
+++ lib/coler.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static Color White = Color.fromARGB(255, 249, 247, 247);
+  static Color Grey = Color.fromARGB(255, 219, 226, 239);
+
+  static Color Blue = Color.fromARGB(255, 63, 114, 175);
+  static Color Navy = Color.fromARGB(255, 17, 45, 78);
+
+  static Color Teal = Color.fromARGB(255, 0, 173, 181);
+  static Color Black = Color.fromARGB(255, 34, 40, 49);
+
+  static Color Dark = Color.fromARGB(255, 57, 62, 70);
+  static Color Red = Color.fromARGB(255, 185, 36, 36);
+
+  TextStyle word =
+      TextStyle(color: White, fontWeight: FontWeight.bold, fontSize: 40);
+  TextStyle grantitel =
+      TextStyle(color: Black, fontWeight: FontWeight.bold, fontSize: 32);
+  TextStyle sheptitel =
+      TextStyle(color: Black, fontWeight: FontWeight.bold, fontSize: 32);
+  TextStyle normalp =
+      TextStyle(color: Dark, fontWeight: FontWeight.w500, fontSize: 22);
+  TextStyle redp =
+      TextStyle(color: Dark, fontWeight: FontWeight.w500, fontSize: 22);
+}
